560d5c3870b6ae335848690a582be42f668d9b83
trevorswan11 <tcs94@case.edu>
2026-02-01 18:29:22 -0500

    add nix flake for reproducable builds

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            git
+            cmake
+            ninja
+            clang-tools
+          ];
+        };
+      }
+    );
+}
