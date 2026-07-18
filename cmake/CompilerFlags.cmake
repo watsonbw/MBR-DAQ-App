a5e15577338f9e67b0e412e695c16eb3bf4260a4
trevorswan11 <tcs94@case.edu>
2026-07-18 14:42:03 -0400

    chore: Bump C++ standard and alter README

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -11,7 +11,7 @@ if(MINGW)
     add_link_options("-static-libgcc" "-static-libstdc++" "-static")
 endif()
 
-set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
